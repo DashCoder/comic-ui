eeb932a97dcc06b9e4a527b5f31eedafa45a1985
Axel Sunnehall <axels@dashup.co>
2024-09-07 19:52:37 +0100

    remove superfluous code

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,8 @@
 import "../styles/global.css";
-import { useRouter } from "next/router";
 import Head from "next/head";
 import Layout from "../components/layout";
-import { Inter, Roboto_Mono } from 'next/font/google'
-import { Html } from "next/document";
- 
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-  display: 'swap',
-})
- 
-const roboto_mono = Roboto_Mono({
-  subsets: ['latin'],
-  variable: '--font-roboto-mono',
-  display: 'swap',
-})
 
 export default function MyApp({ Component, pageProps }) {
-    const router = useRouter();
 
     return (
     <>
